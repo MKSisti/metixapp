0b48cc0cafd63f9c127b9394c1a8fcc32138561c
dvnkboi <aymandb@outlook.com>
2021-03-11 18:13:06 +0100

    scrolling body

--- src/components/BaseComponent/BaseBody.vue
+++ src/components/BaseComponent/BaseBody.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class=" relative h-full">
-    <div class="flex justify-between">
+  <div class=" relative h-screen">
+    <div class="flex justify-between h-1/5">
       <div>
         <span class="text-4xl font-bold flex justify-start items-center flex-row relative">
           <slot name="title"></slot>
@@ -11,7 +11,10 @@
       </div>
       <slot name="topRight"></slot>
     </div>
-    <div>
+    <div class="w-full">
+      <div class="w-full h-px bg-black-light-15 opacity-10"></div>
+    </div>
+    <div class="h-4/5 overflow-auto z-10">
       <slot name="content"></slot>
     </div>
   </div>
