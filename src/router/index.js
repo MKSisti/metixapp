15da77498e62dd4bdbd64c82f6fe887a9d755c39
dvnkboi <aymandb@outlook.com>
2021-03-13 00:02:02 +0100

    poopy 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,40 @@
 import { createRouter, createWebHistory } from "vue-router";
-import routes from './routes.js'
+import routes from "./routes.js";
+import localForage from "localforage";
 // import Home from "../views/Home.vue";
-
 
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes
 });
 
+router.beforeEach(async (to, from, next) => {
+  const keys = await localForage.keys();
+
+  //if route has groupId param
+  if (to.params.GroupId) {
+
+    //if groupId param in store
+    if(keys.includes('group_' + to.params.GroupId)){
+      console.log('group exists');
+
+      // if route has moduleId param
+      if(to.params.ModuleId){
+
+        //if moduleId param in store
+        if(keys.includes('module_' + to.params.ModuleId)) next();
+        else next('/');
+      }
+      else{
+        next();
+      }
+    }
+    else{
+      next('/');
+    }
+  }
+  else next();
+
+});
 
 export default router;
