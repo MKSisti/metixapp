2499b1ddc7ad282063666b0a6d08374524cd0882
dvnkboi <aymandb@outlook.com>
2021-03-14 20:41:29 +0100

    poopy animations and polish

--- src/views/SingleModule.vue
+++ src/views/SingleModule.vue
@@ -12,26 +12,33 @@
     </template>
 
     <template v-slot:content>
-      <div :class="{ 'flex-col': tests && tests?.length >= 1 }" class="w-full mt-28 flex justify-start items-start space-y-2 px-3 overflow-hidden relative">
+      <div class="w-full mt-28 space-y-2 px-3 overflow-hidden relative flex-col">
         <!-- new test -->
-        <div class="bg-black-light-1 flex justify-between items-center flex-row w-64 h-16 p-4 text-2xl">
-          <h1 class="capitalize font-bold">new Test</h1>
-          <span @click="addNewTest" class="cursor-pointer hover:text-blue-light-1 text-3xl font-bold bg-black-light-5 text-blue-base w-9 h-9 flex justify-center items-center p-px">
-            <box-icon name="plus" size="cssSize" class="w-full h-full fill-current" v-pre></box-icon>
-          </span>
-        </div>
-
-        <!-- user hints -->
-        <div v-if="!tests || tests?.length < 1" class="font-bold text-4xl pl-4 flex justify-center items-center">
-          <box-icon name="arrow-back" size="cssSize" class="w-8 h-8 fill-current mt-1 mr-2" v-pre></box-icon>
-          <h1>Add your first test</h1>
+        <div class="w-full flex justify-start items-center relative">
+          <div class="bg-black-light-1 flex justify-between items-center flex-row w-64 h-16 p-4 text-2xl">
+            <h1 class="capitalize font-bold">new Test</h1>
+            <span @click="addNewTest" class="cursor-pointer hover:text-blue-light-1 text-3xl font-bold bg-black-light-5 text-blue-base w-9 h-9 flex justify-center items-center p-px">
+              <box-icon name="plus" size="cssSize" class="w-full h-full fill-current" v-pre></box-icon>
+            </span>
+          </div>
+          <transition @before-leave="beforeLeave" name="fade-x" appear>
+            <!-- user hints -->
+            <div v-show="!tests || tests?.length < 1" class="font-bold text-4xl pl-4 flex justify-center items-center transition-all duration-200 transform-gpu">
+              <box-icon name="arrow-back" size="cssSize" class="w-8 h-8 fill-current mt-1 mr-2" v-pre></box-icon>
+              <h1>Add your first test</h1>
+            </div>
+          </transition>
         </div>
 
         <!-- test display loop -->
-        <div v-else class="w-full relative space-y-2">
-          <transition-group @before-leave="beforeLeave" name="fade-x" mode="out-in" appear>
-            <base-test class="transition-all duration-200 transform-gpu" v-for="(test) in tests" :key="test.id" :test="test" :gid="GroupId" />
-          </transition-group>
+        <div class="relative w-full">
+          <transition @before-leave="beforeLeave" name="fade-x" appear>
+            <div v-if="tests && tests?.length >= 1" class="w-full relative space-y-2 transition-all duration-200 transform-gpu">
+              <transition-group @before-leave="beforeLeave" name="fade-x" appear>
+                <base-test class="transition-all duration-200 transform-gpu" v-for="test in tests" :key="test.id" :test="test" :gid="GroupId" />
+              </transition-group>
+            </div>
+          </transition>
         </div>
       </div>
     </template>
@@ -76,7 +83,7 @@
           gid: this.GroupId,
           mid: this.ModuleId,
         });
-      }
+      },
     },
   };
 </script>
